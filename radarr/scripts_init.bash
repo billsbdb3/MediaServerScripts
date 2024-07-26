88f84ca1cb4a8746965b806b09d35cfce314aab5
billsbdb3 <64713235+billsbdb3@users.noreply.github.com>
2024-07-25 21:20:43 -0400

    Update scripts_init.bash

--- radarr/scripts_init.bash
+++ radarr/scripts_init.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/with-contenv bash
-curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/radarr/setup.bash | bash
+curl https://raw.githubusercontent.com/billsbdb3/MediaServerScripts/main/radarr/setup.bash | bash
 exit
